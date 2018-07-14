c6d5985fe72c858469e3c0648d2ca80588e31404
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2018-07-14 23:29:01 +0300

    0.0.4: update bot list from blocktogether

--- metabotTwitter/bot_accounts.js
+++ metabotTwitter/bot_accounts.js
@@ -1,5 +1,5 @@
 /**
- * @updated: 2018-07-14 20:28:36
+ * @updated: 2018-07-14 20:29:01
  * @source  https://blocktogether.org/show-blocks/SiJai3FyVmodO0XxkL2r-pezIK_oahHRwqv9I6U3
 */
 var BOT_ACCOUNTS = {
