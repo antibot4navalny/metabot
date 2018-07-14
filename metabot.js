73ad12086786ddb9c6a206f1c9b2f80eb89ef38b
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2018-07-14 03:10:47 +0300

    0.0.2: +links to Report a bot / Suggest vindicating (unlisting) a user

--- metabot.js
+++ metabot.js
@@ -23,7 +23,7 @@ if(JSON.parse(document.querySelector('#init-data').value)["night_mode_activated"
 
 function tick()
 {
-	var a=document.querySelectorAll('div.tweet')
+	var a=document.querySelectorAll('#permalink-overlay div.tweet')
 
 	var i, x, t
 	for (i = 0; i < a.length; i++)
@@ -31,12 +31,14 @@ function tick()
 	{
 		// выделяем все твиты, показанные на странице
 		x=Number(a[i].getAttribute("data-user-id"))
+		var actionName=""
+		
+		t = a[i]
 		
 		if (BOT_ACCOUNTS[x]) // проверка, есть ли пользователь в списке известных ботов
 		{
 			// подсвечиваем весь твит стилем is_bot
 			//a[i].innerHTML += '&nbsp;[БОТ]'
-			t = a[i]
 			t.className += ' is_bot'
 			
 			// дописываем "БОТ: " перед именем автора твита
@@ -47,7 +49,22 @@ function tick()
 			// делаем текст твита более блёклым
 			tweetTxt=t.querySelector('div.js-tweet-text-container p.tweet-text')
 			tweetTxt.className = 'bot_text ' + tweetTxt.className
+			
+			menuAction = "Предложить реабилитировать"
+			dmAction = "Реабилитировать из ботов"
+		} else {
+			menuAction = "Сообщить о кремлеботе"
+			dmAction = "В кремлеботы"
 		}
+		// добавляем пункт меню "Пожаловаться / Реабилитировать" 
+		twtBtn=t.querySelector('div.ProfileTweet-action div.dropdown-menu ul')
+
+		tweetPermalink=(new URL(t.getAttribute("data-permalink-path") , document.location)).href
+		// screenName=t.getAttribute("data-screen-name")
+		// fullName=t.getAttribute("data-name")
+		twtBtn.innerHTML +='<li class="dropdown-divider" role="presentation"></li>'
+		twtBtn.innerHTML +="<li role='presentation'><a href=\"https://twitter.com/messages/compose?recipient_id=973677193816571905&text="+encodeURIComponent(tweetPermalink + "\n" + dmAction + ", вот почему:\n") + "\" target=\"_blank\">"+menuAction+"</a></li>"
+		
 		// помечаем твит как обработанный, чтобы впредь на него не тратить время
 		a[i].dataset.mt_is_upd = 1
 	}
