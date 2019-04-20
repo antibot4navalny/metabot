79a9e11595be55bec3efa1553f5b5f1877ebc724
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2019-04-20 11:44:53 +0300

    make sure internet connection is up; make the script completely autonomous; add updating Mozilla extension

--- update_bot_accounts.sh
+++ update_bot_accounts.sh
@@ -4,14 +4,20 @@ cd "$SCRIPTPATH"
 
 csv_db_URL="https://blocktogether.org/show-blocks/SiJai3FyVmodO0XxkL2r-pezIK_oahHRwqv9I6U3"
 
-curl --silent "$csv_db_URL".csv > bot_accounts.csv
+../common/sh/utils/ensure_internet_connection.sh
+while
+	../../
+	curl --silent "$csv_db_URL".csv > bot_accounts.csv
+	[ ! -s "bot_accounts.csv" ]		# The loop ending test
+do :; done
 
 if cmp --quiet bot_accounts.csv bot_accounts.previous.csv; then
 	echo "No changes in DB to push"
 	exit 1
 else
-	echo "DB changes detected, preparing to push them. Sounds right?"
-	read -n 1 -s
+	echo "DB changes detected, preparing to push them."
+# 	echo Sounds right?"
+# 	read -n 1 -s
 
 	echo "/**" > bot_accounts.js
 	date -u "+ * @updated: %Y-%m-%d %H:%M:%S" >> bot_accounts.js
@@ -29,8 +35,9 @@ else
 		>> bot_accounts.js
 	echo "}" >> bot_accounts.js
 	
-	echo "DB updated. Ready to continue?"
-	read -n 1 -s	
+	echo "DB updated."
+# 	echo "Ready to continue?"
+# 	read -n 1 -s	
 
 	mv bot_accounts.js metabotTwitter
 
@@ -57,11 +64,13 @@ else
 	git add metabotTwitter.crx updates.xml next_version.txt metabotTwitter/manifest.json metabotTwitter/bot_accounts.js
 
 	git commit -m "$version: update bot list from blocktogether"
-	echo "About to push DB changes. Sounds good?"
-	read -n 1 -s
+	echo "About to push DB changes."
+# 	echo "Sounds good?"
+# 	read -n 1 -s
 	git push
 
 	./update_chrome_web_store_version.sh
+	./update_mozilla_addons_version.sh
 
 	mv bot_accounts.csv bot_accounts.previous.csv
 	exit 0
