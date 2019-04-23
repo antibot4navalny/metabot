1e5307de0ddd1cd7ce257fbe4ee1e94e798e9024
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2019-04-23 16:36:42 +0300

    encapsulate building packaged extension into a dedicated script

--- update_bot_accounts.sh
+++ update_bot_accounts.sh
@@ -12,47 +12,7 @@ else
 # 	echo Sounds right?" >&2
 # 	read -n 1 -s
 
-	echo "/**" > bot_accounts.js
-	date -u "+ * @updated: %Y-%m-%d %H:%M:%S" >> bot_accounts.js
-	echo " * @source " $csv_db_URL >> bot_accounts.js
-	echo "*/" >> bot_accounts.js
-
-	echo "var BOT_ACCOUNTS = {" >> bot_accounts.js
-	sed -e 's/$/: {},/' bot_accounts.csv >> bot_accounts.js
-	echo "}" >> bot_accounts.js
-
-	echo "var SCREEN_NAMES = {" >> bot_accounts.js
-	tt-users-get-hydrated --user-ids bot_accounts.csv |
-		jq '. | .screen_name' |
-		sed -e 's/$/: {},/' \
-		>> bot_accounts.js
-	echo "}" >> bot_accounts.js
-	
-	echo "DB updated." >&2
-# 	echo "Ready to continue?" >&2
-# 	read -n 1 -s	
-
-	mv bot_accounts.js metabotTwitter
-
-	version=`cat next_version.txt`
-	
-	
-	sed -E "s/VVVVVV/$version/" manifest.template.json > MetabotTwitter/manifest.json
-	sed -E "s/VVVVVV/$version/" updates.template.xml > updates.xml
-
-	extensionator -o ./metabotTwitter.crx -i metabotTwitter.pem -d metabotTwitter -e .DS_Store
-
-#	 Previously:
-# 	 "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --pack-extension=metabotTwitter --pack-extension-key=metabotTwitter.pem --profile-directory="Profile 12"
-
-
-	grep --invert-match '"update_url":' metabotTwitter/manifest.json > "for_Chrome_Store/manifest.json"
-	rm metabotTwitter.zip
-	zip --recurse-paths metabotTwitter.zip metabotTwitter for_Chrome_Store --exclude "metabotTwitter/manifest.json" --exclude "*/.DS_Store" --junk-paths
-
-	## increment the rightmost component of version (assuming there is has at least two components)
-	next_version="${version%.*}.$((${version##*.}+1))"
-	echo $next_version > next_version.txt
+	./build_packaged_extension.sh
 
 	git add metabotTwitter.crx updates.xml next_version.txt metabotTwitter/manifest.json metabotTwitter/bot_accounts.js
 
