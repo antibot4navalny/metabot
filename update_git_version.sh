17f6fdceb0b9837b2501ec70f343e732c895547b
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2019-05-05 00:13:31 +0300

    rm version number: redundant in routine list updates

--- update_git_version.sh
+++ update_git_version.sh
@@ -1,6 +1,6 @@
 	git add metabotTwitter.crx updates.xml next_version.txt metabotTwitter/manifest.json metabotTwitter/bot_accounts.js
 
-	git commit -m "$version: update list of known bots"
+	git commit -m "update list of known bots"
 	echo "About to push DB changes." >&2
 # 	echo "Sounds good?" >&2
 # 	read -n 1 -s
