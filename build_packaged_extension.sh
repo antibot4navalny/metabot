4288cfb074b3ee7de764bf30f84d901b055bffd3
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2019-05-05 00:05:05 +0300

    extract building list of bots into a dedicated script

--- build_packaged_extension.sh
+++ build_packaged_extension.sh
@@ -1,27 +1,4 @@
-	echo "/**" > bot_accounts.js
-	date -u "+ * @updated: %Y-%m-%d %H:%M:%S" >> bot_accounts.js
-
-	if [ ! -z ${csv_db_URL+x} ];	# if variable set
-		echo " * @source " "$csv_db_URL" >> bot_accounts.js
-	fi
-	echo "*/" >> bot_accounts.js
-
-	echo "var BOT_ACCOUNTS = {" >> bot_accounts.js
-	sed -e 's/$/: {},/' bot_accounts.csv >> bot_accounts.js
-	echo "}" >> bot_accounts.js
-
-	echo "var SCREEN_NAMES = {" >> bot_accounts.js
-	tt-users-get-hydrated --user-ids bot_accounts.csv |
-		jq '. | .screen_name' |
-		sed -e 's/$/: {},/' \
-		>> bot_accounts.js
-	echo "}" >> bot_accounts.js
-	
-	echo "DB updated." >&2
-# 	echo "Ready to continue?" >&2
-# 	read -n 1 -s	
-
-	mv bot_accounts.js metabotTwitter
+	. ./build_bot_accounts_js.sh
 
 	sed -E "s/VVVVVV/$version/" manifest.template.json > MetabotTwitter/manifest.json
 	sed -E "s/VVVVVV/$version/" updates.template.xml > updates.xml
