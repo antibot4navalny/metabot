c47f57f0eb22629766a233abe260ac4549583fb9
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2019-05-05 00:12:26 +0300

    generalize commit message to non-blocktogether updates

--- update_git_version.sh
+++ update_git_version.sh
@@ -1,6 +1,6 @@
 	git add metabotTwitter.crx updates.xml next_version.txt metabotTwitter/manifest.json metabotTwitter/bot_accounts.js
 
-	git commit -m "$version: update bot list from blocktogether"
+	git commit -m "$version: update list of known bots"
 	echo "About to push DB changes." >&2
 # 	echo "Sounds good?" >&2
 # 	read -n 1 -s
