83df574ade83763edb60f9a90d2f55fcaca3762c
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2019-07-27 03:52:19 +0300

    update list of known bots

--- metabotTwitter/bot_accounts.js
+++ metabotTwitter/bot_accounts.js
@@ -1,11 +1,11 @@
 /**
- * @updated: 2019-07-26 18:40:08
+ * @updated: 2019-07-27 00:52:17
 */
 var BOT_ACCOUNTS = {
 <html>: {},
-<head><title>504 Gateway Time-out</title></head>: {},
+<head><title>502 Bad Gateway</title></head>: {},
 <body bgcolor="white">: {},
-<center><h1>504 Gateway Time-out</h1></center>: {},
+<center><h1>502 Bad Gateway</h1></center>: {},
 <hr><center>nginx/1.10.3 (Ubuntu)</center>: {},
 </body>: {},
 </html>: {},
