21a6f1359de73c6a728c4ca32916d85203be0b8f
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2019-10-15 23:26:39 +0300

    0.6.0: switch to Google Forms for tweet reports

--- metabotTwitter/url_templates.js
+++ metabotTwitter/url_templates.js
@@ -1,12 +1,16 @@
 // шаблоны ссылок на "Сообщить о боте"
 
-var templateReportURLPrefix = "https://twitter.com/messages/compose?recipient_id=1067060314884190209&text=${tweetInvokedFrom}"
-var reportType=""
+var templateReportTweetUrl='https://docs.google.com/forms/d/e/'+
+ 	'1FAIpQLSfPoe1MnSm_YNroehp6mLVgLYyVWWl-kzoeD13hAkEcDhG0QQ'+
+	'/viewform?usp=pp_url'+
+	'&entry.44520066=${reportedAccount}'+
+	'&entry.2043639068=${reportingUser}'+
+	'&entry.63366018=${tweetInvokedFrom}'+
+	'&entry.1893779955=${tweetInvokedFrom}';
 
-dmAction = "В кремлеботы"
-var templateReportTweetUrl=templateReportURLPrefix + 
-	encodeURIComponent("\n" + dmAction + ", вот почему:\n")
-
-dmAction = "Это не кремлебот"
-var templateReportUntypicalTweetUrl=templateReportURLPrefix + 
-	encodeURIComponent("\n" + dmAction + ", вот почему:\n")
+var templateReportUntypicalTweetUrl='https://docs.google.com/forms/d/e/'+
+	'1FAIpQLSc4HYezRxBA_b8Ywe_CKjwa2_QI-uMjwwtfjUMXze6Mw-ayOg'+
+	'/viewform?usp=pp_url'+
+	'&entry.1071230723=${reportedAccount}'+
+	'&entry.560365343=${tweetInvokedFrom}'+
+	'&entry.221506714=${reportingUser}'
