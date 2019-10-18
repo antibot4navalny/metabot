d23683f5e495c4f2cc625cc528574b82110f0077
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2019-10-18 19:52:26 +0300

    create separate unpacked copy for Chrome and Opera; remove Manifest fields not supported by Firefox

--- build_packaged_extension.sh
+++ build_packaged_extension.sh
@@ -6,21 +6,29 @@
 
 # For Firefox, save a copy with full manifest
 	cp metabotTwitter/* Firefox_readonly_copy
+	grep --invert-match \
+		--regexp='"update_url":' \
+		--regexp='"hot-reload.js",' \
+		"metabotTwitter/manifest.json" > \
+		"Firefox_readonly_copy/manifest.json"
+	zip FirefoxTestBuild.zip Firefox_readonly_copy/* --junk-paths
+
 
 # For Chrome and Opera, strip unsupported feild
-	grep --invert-match --regexp='"browser_specific_settings":' "Firefox_readonly_copy/manifest.json" > "metabotTwitter/manifest.json"
+	cp metabotTwitter/* ChromeOpera_readonly_copy
+	grep --invert-match --regexp='"browser_specific_settings":' "metabotTwitter/manifest.json" > "ChromeOpera_readonly_copy/manifest.json"
 
 # For Opera self-distribution of CRX via GitHub
 	rm metabotTwitter.crx
-	extensionator -o ./metabotTwitter.crx -i metabotTwitter.pem -d metabotTwitter -e .DS_Store -e .web-extension-id
+	extensionator -o ./metabotTwitter.crx -i metabotTwitter.pem -d ChromeOpera_readonly_copy -e .DS_Store -e .web-extension-id
 #	 Previously:
 # 	 "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --pack-extension=metabotTwitter --pack-extension-key=metabotTwitter.pem --profile-directory="Profile 12"
 
 
 # For Chrome, remove update_url which is only necessary for Opera self-hosting
-	grep --invert-match --regexp='"update_url":' --regexp='"browser_specific_settings":' "Firefox_readonly_copy/manifest.json" > "metabotTwitter/manifest.json"
+	grep --invert-match --regexp='"update_url":' --regexp='"browser_specific_settings":' "metabotTwitter/manifest.json" > "ChromeOpera_readonly_copy/manifest.json"
 
 
 # For Chrome
 	rm metabotTwitter.zip
-	zip --recurse-paths metabotTwitter.zip metabotTwitter --exclude "*/.DS_Store" --exclude "metabotTwitter/.web-extension-id" --junk-paths
+	zip --recurse-paths metabotTwitter.zip ChromeOpera_readonly_copy --exclude "*/.DS_Store" --exclude "ChromeOpera_readonly_copy/.web-extension-id" --junk-paths
