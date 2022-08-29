c223b8906a016886a3e0cd3d65f4ee49df233462
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2022-08-29 23:37:24 +0300

    fix bugs

--- build_scripts/retrieve_bots_list.sh
+++ build_scripts/retrieve_bots_list.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . ../../common/sh/utils/common_funcs.sh
-. build_scripts/credentials/set_remote_source_for_bots.sh
+. credentials/set_remote_source_for_bots.sh
 
-labels_file="assets/labels.json"
+labels_file="labels.json"
 
 scp -p \
   "$remote_username"@"$remote_host":"$remote_location/$labels_file" \
