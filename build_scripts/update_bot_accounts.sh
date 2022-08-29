9f0e6acb6b26dd117bca4c5c8b67b5f26535af61
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2022-08-30 00:04:26 +0300

    stop script if error occurs at any stage

--- build_scripts/update_bot_accounts.sh
+++ build_scripts/update_bot_accounts.sh
@@ -2,9 +2,9 @@ SCRIPTPATH=$(dirname "$0")
 echo "Switching to script folder:""$SCRIPTPATH" >&2
 cd "$SCRIPTPATH"
 
-# ./build_scripts/download_current_blocktogether_blacklist.sh
-# ./build_scripts/get_bots_locally.sh
-  ./build_scripts/retrieve_bots_list.sh
+# ./build_scripts/download_current_blocktogether_blacklist.sh &&
+# ./build_scripts/get_bots_locally.sh &&
+  ./build_scripts/retrieve_bots_list.sh &&
 
 if cmp --quiet assets/labels.json assets/labels.previous.json; then
 	echo "No changes in DB to push" >&2
@@ -14,13 +14,13 @@ else
 # 	echo Sounds right?" >&2
 # 	read -n 1 -s
 
-	./build_scripts/build_packaged_extension.sh
-	./build_scripts/increment_extension_version.sh
+	./build_scripts/build_packaged_extension.sh &&
+	./build_scripts/increment_extension_version.sh &&
 
-# 	./build_scripts/update_git_version.sh
-# 	./build_scripts/update_chrome_web_store_version.sh
-# 	./build_scripts/update_mozilla_addons_version.sh
+# 	./build_scripts/update_git_version.sh &&
+# 	./build_scripts/update_chrome_web_store_version.sh &&
+# 	./build_scripts/update_mozilla_addons_version.sh &&
 
-	cp assets/labels.json assets/labels.previous.json
+	cp assets/labels.json assets/labels.previous.json &&
 	exit 0
 fi
