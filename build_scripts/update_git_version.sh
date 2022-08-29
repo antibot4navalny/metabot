c164dde38360fb5902a5dcacfc085fbde44aee81
antibot4navalny <41168195+antibot4navalny@users.noreply.github.com>
2022-08-29 19:41:50 +0300

    further: removing metabotTwitter/manifest.json:

--- build_scripts/update_git_version.sh
+++ build_scripts/update_git_version.sh
@@ -1,4 +1,4 @@
-	git add releases/metabotTwitter.crx "channel_specific/updates.xml" next_version.txt metabotTwitter/labels.json metabotTwitter/manifest.json
+	git add releases/metabotTwitter.crx "channel_specific/updates.xml" next_version.txt metabotTwitter/labels.json
 
 	git commit -m "update list of known bots"
 	echo "About to push DB changes." >&2
